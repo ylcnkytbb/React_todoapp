c60b858b744558b8416738c91917f3d46cb89b4e
Tuğba Yalçınkaya <tugbayalcinkaya3437@gmail.com>
2022-11-10 14:36:03 +0300

    test-todo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
+/* import Icons from './assets/Icons'
+ */
 /* import background from "./background.jpeg"; */
 /* import background from "./assets/background.jpeg"; */
 
 const INITIAL_STATE = [
-  { id: 1, task: "Okula Git", finished: true },
-  { id: 2, task: "Fatura ode", finished: true },
+  { id: 1, task: "TodoListi tamamla", finished: true },
+  { id: 2, task: "Admin paneli tamamla", finished: true },
+  { id: 3, task: "Ekmek al", finished: true },
 ];
 
+/* ******** local storage ******** */
+
+/* useEffect(() => {
+  localStorage.setItem('list' ,JSON.stringify(list))
+}
+) */
+
+function fonk1()
+{
+  window.alert('Merhaba');
+}
 export default function App() {
   const [list, setList] = useState(INITIAL_STATE);
   const [newTitle, setNewTitle] = useState("");
+  /* const updateTask = (id, updatedTask) => {
+    setTasks(tasks.map((task)) => 
+      (task.id === id ? updatedTask : task)))
+  } */
 
   return (
-    <div className="body" id="img" /* style={
-     {
-        background: `url(${background})`
-      }
-    } */ >  
+    <div className="body" id="img">  
     <div className="App">
       <h1 className="title_h1">What is your planning today?</h1>
       <div className="add_form">
-      g
         <input
           value={newTitle}
           onChange={(e) => setNewTitle(e.target.value)}
-          placeholer="Add a todo"
-        />
+          placeholer="Add a todo.."
+        /> 
         <button
           onClick={() =>
             {
@@ -37,8 +50,7 @@ export default function App() {
           setNewTitle("");
           }
           }
-        >
-          Add
+        >Add
         </button>
       </div>
       <div className="list">
@@ -62,8 +74,17 @@ export default function App() {
           )
         )}
       </div>
+      <div>
+      <div className="delete_btn">
       <button onClick={() => setList(list.filter(item => !item.finished)) } 
-      className="delete">Clear All</button>
+      className="delete"> Clear All
+      </button>
+
+      {/* <div class="alert alert-success" role="alert">
+     A simple success alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+</div> */}
+      </div>
+      </div>
     </div>
     </div>
   );
---
+++ src/assets/Icons.jsx
@@ -0,0 +1,12 @@
+/* import React from 'react'
+import { FaBeer } from 'react-icons/fa';
+
+function Icons() {
+  return (
+    <div>
+        <Icons/>
+    </div>
+  )
+}
+
+export default Icons */
